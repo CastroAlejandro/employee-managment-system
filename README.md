8c4ec5ec5990a7c6796d07b097f66fd236b4e2d3
Alejandro Castro <castroal96@gmail.com>
2021-02-01 08:40:45 -0500

    readme.md added

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # employee-managment-system
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+The application allows the user to keep provide concurrent employee information. The purpose of the assignment was to demonstrate the use of  node.js and mySQL, while implementing CRUD to render an informational system companies can use for employees through a command line applications.
+
+## Table of Contents 
+* [Installation](#installation)
+​
+* [Usage](#usage)
+​
+* [License](#license)
+​
+* [Contributing](#contributing)
+​
+* [Tests](#tests)
+​
+* [Questions](#questions)
+​
+## Installation
+
+​To install necessary dependecies please use the following commands:
+
+	npm i 
+	
+## Usage
+
+	To run with either nodemon or node.js use the folliwng:
+	nodemon app.js || node app.js 
+​​
+## License
+MIT
+	
+
+## Contributing
+​
+You may not contribute to the repo at this time.
+​
+## Tests
+
+    npm test
+
+## Preview
+
+![alt text](preview.png)
+
+## Questions
+​
+If you have any questions, comments, or concerns please feel free to contact me at castroal96@gmail.com or on Github: [CastroAlejandro](https://github.com/CastroAlejandro).
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,34 +2,34 @@ USE employees;
 
 INSERT INTO department (name)
 VALUES
-("Accounting"),
-("Engineering"),
+("FOH"),
+("BOH"),
+("Delivery"),
 ("Legal"),
-("Sales"),
-("Assembly Line");
+("Marketing");
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Accountant Manager', 75000, 1),
-('Accountant', 70000, 1),
-('Lead Engineer', 130000, 2),
-('Software Engineer', 100000, 2),
-('Legal Team Lead', 150000, 3),
-('Lawyer', 90000, 3),
-('Sales Lead', 45000, 4),
-('Salesperson', 35000, 4),
-('Assembly Lead', 50000, 5),
-('Assembler I', 28000, 5),
-('Assembler II', 34000, 5);
+('Server', 24000, 1),
+('Busser', 16000, 1),
+('Host', 14000, 1),
+('Prep', 17000, 2),
+('Line Cook', 18000, 2),
+('Sous Chef', 20000, 2),
+('Head Chef', 45000, 2),
+('Driver', 18000, 3),
+('Lawyer', 80000, 4),
+('Esquire', 60000, 4),
+('Promoter', 30000, 5);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-("Sara", "Martinez", 1, NULL),
-("Adalberto", "Vargas", 2, NULL),
-("Kendrick", "Lamar", 3, NULL ),
-("Xavier", "Torres", 4, NULL),
-("Issac", "Clark", 5, NULL),
-("Emanuel", "Acevedo", 6, NULL),
-("Anakin", "Skywalker", 7, NULL),
-("Angel", "Hernandez", 8, NULL),
-("Sergei", "Dragunov", 9, NULL),
-("Reiner", "Braun", 10, NULL),
-("Jonathan", "Cruz", 11, NULL);
+("Alejandro", "Castro", 1, NULL),
+("Mikayla", "Smith", 2, NULL),
+("Alicia", "Welch", 3, NULL ),
+("Kathy", "Johnson", 4, NULL),
+("Dianna", "Lark", 5, NULL),
+("Florian", "Ocean", 6, NULL),
+("Zeb", "Szen", 7, NULL),
+("Marty", "Jenner", 8, NULL),
+("Mark", "Lofter", 9, NULL),
+("Bill", "Burns", 10, NULL),
+("Sandra", "Crood", 11, NULL);
